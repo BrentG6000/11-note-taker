4f1d31bbda5960079b8ff79fe8448301d3057337
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2022-10-17 00:35:22 -0500

    Added code for post in server.js

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,26 +1,45 @@
 const express = require('express');
+const app = express();
 const fs = require('fs');
 const path = require('path');
-
-const app = express();
-const PORT = 3001;
-
+const PORT = process.env.port || 3001;
 let notesPath = path.join(__dirname, '..', '..', 'notes.html');
-let indexPath = path.join(__dirname, '..', '..', 'index.html')
-// Middleware needed?
+let indexPath = path.join(__dirname, '..', '..', 'index.html');
+let dbPath = path.join(__dirname, '..', '..', '..', 'db', 'db.json');
 
-app.get('/notes', (req, res) => res.sendFile(notesPath));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('./notes', (req, res) => res.sendFile(notesPath));
+
+app.get('./api/notes', (req, res) => {
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+        err ? console.error(err) : res.send(data)
+    });
+});
 
 app.get('*', (req, res) => res.sendFile(indexPath));
 
-/* Example get and post
-app.get('/', (req, res) => res.json(`GET route`));
-app.post('/', (req, res) => res.json(`POST route`));
-*/
+app.post('./api/notes', (req, res) => {
+    // fs.writeFile(dbPath, req.body, (err) => {
+    //     if (err)
+    //         console.log(err);
+    //     else {
+    //         res.send(req.body);
+    //     }
+    res.send(req.body);
+});
 
 app.listen(PORT, () =>
     console.log(`Listening for requests on port ${PORT}! 🏎️`)
 );
 
 // To turn on nodemon: npx nodemon server.js
-// Use Insomnia for api calls
+
+// fs.readFile('/Users/joe/test.txt', 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log(data);
+// });
