b2dd8c033b4e0e2d24b6887d9b3f52f7a8e9d4f9
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2022-10-19 20:38:49 -0500

    Added comments

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -8,37 +8,43 @@ let notesPath = path.join(__dirname, '..', '..', 'notes.html');
 let indexPath = path.join(__dirname, '..', '..', 'index.html');
 let dbPath = path.join(__dirname, '..', '..', '..', 'db', 'db.json');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// Middleware
+app.use(express.json()); // Allows json to be read
+app.use(express.urlencoded({ extended: true })); // Allows requests to be read
+app.use(express.static('public')); // Allows access to static files in the public folder
 
+// Returns notes.html
 app.get('/notes', (req, res) => res.sendFile(notesPath));
 
+// Returns notes from db.json
 app.get('/api/notes', (req, res) => {
     fs.readFile(dbPath, 'utf8', (err, data) => {
         err ? console.error(err) : res.send(data)
     });
 });
 
+// All other gets return homepage
 app.get('*', (req, res) => res.sendFile(indexPath));
 
+// Takes note from req.body and saves it db.json. Returns the new note.
 app.post('/api/notes', (req, res) => {
     if (req.body != {}) {
-        let fileData;
+        let fileData; // Will use to hold new note
         fs.readFile(dbPath, 'utf8', (err, data) => {
         if (err) {
             console.log(err);
         }
         else {
-            fileData = JSON.parse(data);
-            let newData = { ...req.body, id: uuidv4() }
-            fileData.push(newData);
+            fileData = JSON.parse(data); // Old notes from db.json saved to fileData
+            let newData = { ...req.body, id: uuidv4() } // Give new note a unique id
+            fileData.push(newData); // Add new note to old notes
+            // New note list is writen to db.json
             fs.writeFile(dbPath, JSON.stringify(fileData, null, 2), (err) => {
                 if (err) {
                     console.log(err);
                 }
                 else {
-                    res.end(JSON.stringify(newData));
+                    res.end(JSON.stringify(newData)); // New note returned
                     console.log("Note logged.");
                 }
             });  
@@ -47,7 +53,6 @@ app.post('/api/notes', (req, res) => {
     else {
         res.status(500).json('Error in posting note');
     }
-    //res.end(JSON.stringify(req.body));
 });
 
 app.listen(PORT, () =>
